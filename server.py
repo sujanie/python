7d31bda0db7799e46ecf2f98b17949dc7f3e0ab0
sujanie <36542245+sujanie@users.noreply.github.com>
2019-05-28 10:45:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,56 +1,60 @@
-#!/usr/bin/python
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import Aggregate
-import concurrency
-import sys
-PORT_NUMBER = 8080
-from urllib.parse import parse_qs
-#This class will handles any incoming request from the browser
-class myHandler(BaseHTTPRequestHandler):
-        #Handler for the GET requests
-        def do_GET(self):
-                path, _, query_string = self.path.partition('?')
-                queryData = parse_qs(query_string)
-                city = queryData.get("city")[0]
-                checkIn = queryData.get("checkIn")[0]
-                checkOut = queryData.get("checkOut")[0]
-                num = queryData.get("num")[0]
-                num1 = queryData.get("num1")[0]
+while True:
+        print("IM RUNNING")
+        
 
-                print (city)
-                print (checkIn)
-                print (checkOut)
-                print (num)
-                print (num1)
+# #!/usr/bin/python
+# from http.server import BaseHTTPRequestHandler, HTTPServer
+# import Aggregate
+# import concurrency
+# import sys
+# PORT_NUMBER = 8080
+# from urllib.parse import parse_qs
+# #This class will handles any incoming request from the browser
+# class myHandler(BaseHTTPRequestHandler):
+#         #Handler for the GET requests
+#         def do_GET(self):
+#                 path, _, query_string = self.path.partition('?')
+#                 queryData = parse_qs(query_string)
+#                 city = queryData.get("city")[0]
+#                 checkIn = queryData.get("checkIn")[0]
+#                 checkOut = queryData.get("checkOut")[0]
+#                 num = queryData.get("num")[0]
+#                 num1 = queryData.get("num1")[0]
 
-                URLS=concurrency.construct_url(city,checkIn,checkOut,num)
-                if __name__ == '__main__':
-                        details=concurrency.main(URLS)
-                        self.send_response(200)
-                        self.send_header('Content-type','text/html')
-                        self.end_headers()
-                        data=Aggregate.aggregate(details)
-                        # Send the html message
+#                 print (city)
+#                 print (checkIn)
+#                 print (checkOut)
+#                 print (num)
+#                 print (num1)
+
+#                 URLS=concurrency.construct_url(city,checkIn,checkOut,num)
+#                 if __name__ == '__main__':
+#                         details=concurrency.main(URLS)
+#                         self.send_response(200)
+#                         self.send_header('Content-type','text/html')
+#                         self.end_headers()
+#                         data=Aggregate.aggregate(details)
+#                         # Send the html message
                         
-                        data=str.encode(data)
+#                         data=str.encode(data)
                         
-                        self.wfile.write(data)
+#                         self.wfile.write(data)
 
 
-def run():
-        try:
-                #Create a web server and define the handler to manage the
-                #incoming request
-                server = HTTPServer(('127.0.0.1', PORT_NUMBER), myHandler)
-                print ("Started httpserver on port " , PORT_NUMBER)
-                #Wait forever for incoming htto requests
-                server.serve_forever()
+# def run():
+#         try:
+#                 #Create a web server and define the handler to manage the
+#                 #incoming request
+#                 server = HTTPServer(('127.0.0.1', PORT_NUMBER), myHandler)
+#                 print ("Started httpserver on port " , PORT_NUMBER)
+#                 #Wait forever for incoming htto requests
+#                 server.serve_forever()
 
-        except KeyboardInterrupt:
-                print ("^C received, shutting down the web server")
-                server.socket.close()
-if __name__ == '__main__':
+#         except KeyboardInterrupt:
+#                 print ("^C received, shutting down the web server")
+#                 server.socket.close()
+# if __name__ == '__main__':
 
-        from sys import argv
+#         from sys import argv
 
-        run()
+#         run()
